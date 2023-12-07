329e0f9522cba4549503d035645fdc2ec53998f7
xing <e96357@gmail.com>
2023-12-07 14:49:07 +0800

    feat: user register

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,71 @@
-import {DatePicker} from "antd";
+import {Button, Form, Input, message} from "antd";
+import './login.css'
+import {login} from "./interfaces";
 
-export function Login(){
-    return <div><DatePicker/></div>
+interface LoginUser {
+    username: string;
+    password: string;
+}
+
+const onFinish = async (values: LoginUser) => {
+    const res = await login(values.username, values.password);
+    console.log(res.status)
+
+    const { code, message: msg, data} = res.data;
+    if (res.status === 200 || res.status === 201) {
+        message.success("登录成功");
+        console.log(res.data);
+        localStorage.setItem('access_token', data.accessToken);
+        localStorage.setItem('refresh_token', data.refreshToken);
+        localStorage.setItem('user_info', JSON.stringify(data.userInfo));
+    } else {
+        message.error(res.data.data || '系统繁忙，请稍后再试');
+    }
+}
+
+const layout1 = {
+    labelCol: {span: 4},
+    wrapperCol: {span: 20}
+}
+
+const layout2 = {
+    labelCol: {span: 0},
+    wrapperCol: {span: 24}
+}
+
+export function Login() {
+    return <div id="login-container">
+        <h1>会议室预定系统</h1>
+        <Form
+            {...layout1}
+            onFinish={onFinish}
+            colon={false}
+            autoComplete={"off"}
+        >
+            <Form.Item
+                label="用户名"
+                name="username"
+                rules={[{required: true, message: "请输入用户名"}]}
+            >
+                <Input/>
+
+            </Form.Item>
+
+            <Form.Item label="密码" name="password" rules={[{required: true, message: "请输入密码"}]}>
+                <Input.Password/>
+            </Form.Item>
+
+
+            <Form.Item {...layout2}>
+                <div className='links'>
+                    <a href="">创建账号</a>
+                    <a href="">忘记密码</a>
+                </div>
+            </Form.Item>
+
+            <Form.Item {...layout2}>
+                <Button className="btn" type="primary" htmlType="submit">登录</Button>
+            </Form.Item>
+        </Form>
+    </div>
 }
